37fc7a6f8f78a7d0dabb6f6c24471deabe49c5b4
suzeraind <nikitas4478@gmail.com>
2025-09-24 19:31:31 +0300

    [fix] format

--- tasks/addTwoNumbers/solution.ts
+++ tasks/addTwoNumbers/solution.ts
@@ -13,13 +13,11 @@ export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNod
   let carry = 0;
 
   while (l1 || l2 || carry) {
-    const val1 = l1 !== null ? l1?.val : 0;
-    const val2 = l2 !== null ? l2?.val : 0;
-
+    const val1 = l1 !== null ? l1.val : 0;
+    const val2 = l2 !== null ? l2.val : 0;
     const sum = val1 + val2 + carry;
 
     carry = Math.floor(sum / 10);
-
     curr.next = new ListNode(sum % 10);
     curr = curr.next;
 
